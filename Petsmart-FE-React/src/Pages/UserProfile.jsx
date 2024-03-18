aab10d2c433bf3a827106f95899c72e2bc40e6e3
Deekshithaaa <deekshithaa127@gmail.com>
2024-03-17 22:52:52 -0500

    Initial commit with project files

---
+++ Petsmart-FE-React/src/Pages/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const UserProfile = () => {
+  return (
+    <>
+      <div className="container-fluid py-5">
+        <div className="container">
+          <div className="border-start border-5 border-primary ps-5 mb-5">
+            <h6 className="text-primary text-uppercase">Services</h6>
+            <h1 className="display-5 text-uppercase mb-0">Our Services</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
